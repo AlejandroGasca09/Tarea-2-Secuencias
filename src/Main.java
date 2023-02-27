ce7b542f494846b1c3b40ed6fbd3736d66b12abb
AlejandroGasca09 <119777270+AlejandroGasca09@users.noreply.github.com>
2023-02-27 01:50:28 -0600

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Introduce una cadena 3 caracteres :");
+        String input= teclado.nextLine();
+        boolean Secuencia = true;
+        for (int i = 0; i < input.length() - 1; i++) {
+            int cadena1 = (int)input.charAt(i);
+            int cadena2 =(int)input.charAt(i+1);
+            if (cadena2-cadena1 !=1){
+                Secuencia = false;
+            }
+        }
+        System.out.println("la secuencia es : " + Secuencia);
     }
 }
